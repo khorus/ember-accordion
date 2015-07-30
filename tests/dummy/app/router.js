05f66829eb3aa179475b2c239a4e1b96b7a33442
Chris Bonser <chris@khorus.com>
2015-07-30 08:51:29 -0500

    wip on building some examples in the dummy app

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('simple');
+  this.route('multiple-pannels-per-item');
+  this.route('extending');
 });
 
 export default Router;
