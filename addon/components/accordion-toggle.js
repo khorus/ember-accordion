1a08561f37ba53689dca867efb1b75979b7dd0ed
Chris Bonser <chris@khorus.com>
2016-02-18 10:03:14 -0600

    - Disabling toggle click event when listId or itemId is null or undefined - Switching sass dependency

--- addon/components/accordion-toggle.js
+++ addon/components/accordion-toggle.js
@@ -4,12 +4,17 @@ import ElementActiveState from '../mixins/element-active-state';
 const AccordionToggleComponent = Ember.Component.extend(ElementActiveState, {
   accordion: Ember.inject.service('accordion'),
   classNames: ["AccordionToggle"],
-  classNameBindings: ['isActive'],
+  classNameBindings: ['isActive', 'isDisabled'],
 
   panelName: 'panel-one',
+  isListIdNone: Ember.computed.none('listId'),
+  isItemIdNone: Ember.computed.none('itemId'),
+  isDisabled: Ember.computed.or('isListIdNone', 'isListIdNone'),
 
   click: function() {
-    this.get('accordion').toggleClick(this.getProperties('listId', 'itemId', 'panelName'));
+    if(!this.get('isDisabled')) {
+      this.get('accordion').toggleClick(this.getProperties('listId', 'itemId', 'panelName'));
+    }
     return false;
   }
 });
