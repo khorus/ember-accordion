c692d0ffcdb934a8b811321d02b3f15e2d8564d7
Chris Bonser <chris@khorus.com>
2016-02-25 16:50:10 -0600

    improving docs

--- tests/dummy/app/controllers/home.js
+++ tests/dummy/app/controllers/home.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   items: [
-    {name: 'thing 1', color: 'red'},
-    {name: 'thing 2', color: 'red too'}
+    {name: 'Section 1', value: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut sagittis enim. Phasellus pharetra arcu ex, vitae lobortis augue tincidunt a. Duis vitae suscipit magna, vel gravida augue. Sed commodo erat purus, eget sodales leo tempus id. In eget nisl nec justo ve'},
+    {name: 'Section 2', value: 'Nam sollicitudin mi sed metus bibendum blandit. Sed fermentum quis risus eget suscipit. Aliquam nec blandit elit. Donec nec semper ante. Donec nec leo dolor. Sed convallis massa erat, non maximus dolor sodales vitae. Duis faucibus tellus ut felis aliquet, non consequat purus porta. In vitae ante sed eros gravida vestibulum. Ut euismod, nibh consequat finibus fringilla, '},
+    {name: 'Section 3', value: 'Mauris vitae urna euismod, feugiat orci eu, tempus velit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Class aptent ta'}
   ]
 });
