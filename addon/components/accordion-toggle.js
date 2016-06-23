97ba0ee3e946d658acb74a70c42a1559224bd7c5
Rob <rhlowe@gmail.com>
2016-06-23 09:19:07 -0500

    [Fixes #121866309]
    Offset the page by one pixel about 250ms later to force rerender in Safari to remove a strange render bug.
    
    TODO: Figure out a way to fix this that isn't coupled to a CSS selector.

--- addon/components/accordion-toggle.js
+++ addon/components/accordion-toggle.js
@@ -14,6 +14,13 @@ const AccordionToggleComponent = Ember.Component.extend(ElementActiveState, {
   click: function() {
     if(!this.get('isDisabled')) {
       this.get('accordion').toggleClick(this.getProperties('listId', 'itemId', 'panelName'));
+
+      // Offset the page by one pixel about 250ms later to force rerender in
+      // Safari to remove a strange render bug.
+      // TODO: Figure out a way to fix this that isn't coupled to a CSS selector.
+      Ember.run.later('Accordion Click', function() {
+        $('#content').offset({ top: $('#content').offset().top - 1 });
+      }, 250);
     }
     return false;
   }
