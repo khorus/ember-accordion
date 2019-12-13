29d915e2fe63e1d803af32ff88f24c363992bb08
Chris Bonser <chris.bonser@gmail.com>
2019-12-13 07:05:48 -0600

    upgrade to ember 3.14

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,10 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
   this.route('home', {path: '/'});
@@ -14,5 +14,3 @@ Router.map(function() {
   this.route('extending');
   this.route('action-curry');
 });
-
-export default Router;
