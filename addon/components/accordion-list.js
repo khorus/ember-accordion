e8fa733a72950b079e0d17e4651417bcf0b7673b
Chris Bonser <chris.bonser@gmail.com>
2019-12-13 06:49:00 -0600

    Remove positional params

--- addon/components/accordion-list.js
+++ addon/components/accordion-list.js
@@ -1,11 +1,11 @@
-import { A } from '@ember/array';
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { isNone, isEqual } from '@ember/utils';
-import layout from '../templates/components/accordion-list';
-import Item from '../utils/item';
+import { A } from "@ember/array";
+import Component from "@ember/component";
+import { computed } from "@ember/object";
+import { isNone, isEqual } from "@ember/utils";
+import layout from "../templates/components/accordion-list";
+import Item from "../utils/item";
 
-const AccordionListComponent = Component.extend({
+export default Component.extend({
   layout,
   classNames: ["AccordionList"],
 
@@ -17,34 +17,43 @@ const AccordionListComponent = Component.extend({
   _registeredItems: null,
   init() {
     this._super(...arguments);
-    this.set('_activeItems', A([]));
-    this.set('_registeredItems', A([]));
+    this.set("_activeItems", A([]));
+    this.set("_registeredItems", A([]));
   },
 
   // If each registered item has at least on panel open, then return true
-  allExpanded: computed('_activeItems.[]', '_registeredItems.[]', function() {
-    let { _activeItems, _registeredItems } = this.getProperties('_activeItems', '_registeredItems');
+  allExpanded: computed("_activeItems.[]", "_registeredItems.[]", function() {
+    let { _activeItems, _registeredItems } = this.getProperties(
+      "_activeItems",
+      "_registeredItems"
+    );
 
-    let anyMissing = _registeredItems.reduce((anyMissing, item) => anyMissing || isNone(_activeItems.findBy('id', item.itemId)), false);
+    let anyMissing = _registeredItems.reduce(
+      (anyMissing, item) =>
+        anyMissing || isNone(_activeItems.findBy("id", item.itemId)),
+      false
+    );
     return !anyMissing;
   }),
 
   actions: {
     toggleItem(itemId, panelName) {
-      const activeItems = this.get('_activeItems');
-      const targetItem = activeItems.findBy('id', itemId);
+      const activeItems = this.get("_activeItems");
+      const targetItem = activeItems.findBy("id", itemId);
 
       // if target item is already active
-      if(targetItem) {
+      if (targetItem) {
         // and the panel is already active
-        if(isEqual(targetItem.get('panel'), panelName)) {
+        if (isEqual(targetItem.get("panel"), panelName)) {
           activeItems.removeObject(targetItem);
         } else {
-          targetItem.set('panel', panelName);
+          targetItem.set("panel", panelName);
         }
       } else {
         // if simultanious active items are not allow, clear array
-        if(!this.get('allowManyActiveItems')) { activeItems.clear(); }
+        if (!this.get("allowManyActiveItems")) {
+          activeItems.clear();
+        }
 
         let newItem = Item.create({ id: itemId, panel: panelName });
         activeItems.addObject(newItem);
@@ -52,35 +61,38 @@ const AccordionListComponent = Component.extend({
     },
 
     closeItem(itemId) {
-      let activeItems = this.get('_activeItems');
-      const activeItem = activeItems.findBy('id', itemId);
+      let activeItems = this.get("_activeItems");
+      const activeItem = activeItems.findBy("id", itemId);
       activeItems.removeObject(activeItem);
     },
 
     expandAll(panelName) {
-      panelName = panelName || 'panel-one';
-      let { _activeItems, _registeredItems } = this.getProperties('_activeItems', '_registeredItems');
+      panelName = panelName || "panel-one";
+      let { _activeItems, _registeredItems } = this.getProperties(
+        "_activeItems",
+        "_registeredItems"
+      );
       _registeredItems.forEach(registeredItem => {
         if (registeredItem.panelName === panelName) {
-          _activeItems.addObject(Item.create({id: registeredItem.itemId, panel: panelName}));
+          _activeItems.addObject(
+            Item.create({ id: registeredItem.itemId, panel: panelName })
+          );
         }
       });
     },
 
     collapseAll() {
-      this.get('_activeItems').clear();
+      this.get("_activeItems").clear();
     },
 
     // private action, allow panel's to register themselves so they can participate in expand/collapse all
     register(itemId, panelName) {
-      this.get('_registeredItems').addObject({itemId, panelName});
+      this.get("_registeredItems").addObject({ itemId, panelName });
     },
 
     // private action
     unregister(itemId, panelName) {
-      this.get('_registeredItems').removeObject({itemId, panelName});
-    },
+      this.get("_registeredItems").removeObject({ itemId, panelName });
+    }
   }
 });
-
-export default AccordionListComponent;
--- addon/components/accordion-toggle.js
+++ addon/components/accordion-toggle.js
@@ -1,31 +1,25 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { isEqual } from '@ember/utils';
-import layout from '../templates/components/accordion-toggle';
+import Component from "@ember/component";
+import { computed } from "@ember/object";
+import { isEqual } from "@ember/utils";
+import layout from "../templates/components/accordion-toggle";
 
-const AccordionToggleComponent = Component.extend({
+export default Component.extend({
   layout,
   classNames: ["AccordionToggle"],
-  classNameBindings: ['isActive', 'disabled'],
+  classNameBindings: ["isActive", "disabled"],
 
   // Input params
   disabled: null,
-  panelName: 'panel-one',
+  panelName: "panel-one",
 
-  isActive: computed('activePanelName', 'panelName', function() {
-    return isEqual(this.get('activePanelName'), this.get('panelName'));
+  isActive: computed("activePanelName", "panelName", function() {
+    return isEqual(this.get("activePanelName"), this.get("panelName"));
   }),
 
   click: function() {
-    if(!this.get('disabled')) {
-      this.get('toggle')(this.get('panelName'));
+    if (!this.get("disabled")) {
+      this.get("toggle")(this.get("panelName"));
     }
     return false;
   }
 });
-
-AccordionToggleComponent.reopenClass({
-  positionalParams: ['activePanelName', 'toggle']
-});
-
-export default AccordionToggleComponent;
