85e8a7e2635d6b662c39bcc1defda25a4f0e810f
Chris Bonser <chris.bonser@gmail.com>
2019-12-13 06:37:02 -0600

    simplify number of test cases

--- config/ember-try.js
+++ config/ember-try.js
@@ -29,30 +29,6 @@ module.exports = async function() {
         }
       },
       {
-        name: "ember-3.1",
-        npm: {
-          devDependencies: {
-            "ember-source": "~3.1.0"
-          }
-        }
-      },
-      {
-        name: "ember-3.2",
-        npm: {
-          devDependencies: {
-            "ember-source": "~3.2.0"
-          }
-        }
-      },
-      {
-        name: "ember-3.3",
-        npm: {
-          devDependencies: {
-            "ember-source": "~3.3.0"
-          }
-        }
-      },
-      {
         name: "ember-lts-3.4",
         npm: {
           devDependencies: {
